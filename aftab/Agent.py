ff0b9a8e61594d4dba8c499bf82a4dde86c76f79
tahashieenavaz <tahashieenavaz@gmail.com>
2026-03-12 14:48:15 +0100

    adds encoder identifier panic

--- aftab/Agent.py
+++ aftab/Agent.py
@@ -1,11 +1,13 @@
 import torch
 from baloot import acceleration_device
+from .helpers import panic_if_encoder_identifier_is_illegal
 
 
 class Agent:
-    def __init__(self):
+    def __init__(self, encoder_identifier: str = "gamma"):
         self.device = acceleration_device()
         torch.set_float32_matmul_precision("high")
+        panic_if_encoder_identifier_is_illegal(encoder_identifier)
 
-    def train(self):
+    def train(self, environment):
         pass
---
+++ aftab/exceptions/__init__.py
@@ -0,0 +1,1 @@
+from .InvalidEncoderIdentifier import InvalidEncoderIdentifier
