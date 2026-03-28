bdcc1eb23a6d5abb7497913ddcc62d113b4b0f5b
tahashieenavaz <tahashieenavaz@gmail.com>
2026-03-28 15:03:01 +0100

    updates Hadamax Gamma based on design choices of the paper

--- aftab/encoders/HadamaxGammaEncoder.py
+++ aftab/encoders/HadamaxGammaEncoder.py
@@ -2,121 +2,105 @@ import torch
 from ..common import LayerNorm2d, ModuleType
 
 
-class HadamardGammaBlockA(torch.nn.Module):
-    def __init__(self, activation: ModuleType):
+class HadamaxLayer(torch.nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        padding,
+        pool_k,
+        pool_s,
+        pool_p=0,
+        activation=torch.nn.GELU,
+    ):
         super().__init__()
-        self.convolutional = torch.nn.Conv2d(4, 32, kernel_size=3, stride=2, padding=1)
-        self.normalization = LayerNorm2d(32)
+        self.conv_a = torch.nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=1,
+            padding=padding,
+        )
+        self.conv_b = torch.nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=1,
+            padding=padding,
+        )
+
+        self.norm_a = LayerNorm2d(out_channels)
+        self.norm_b = LayerNorm2d(out_channels)
+
         self.activation = activation()
 
+        self.pool_k = pool_k
+        self.pool_s = pool_s
+        self.pool_p = pool_p
+
     def forward(self, x):
-        x = self.convolutional(x)
-        x = self.normalization(x)
-        x = self.activation(x)
-        return x
+        a = self.activation(self.norm_a(self.conv_a(x)))
+        b = self.activation(self.norm_b(self.conv_b(x)))
+        x = a * b
+        return torch.nn.functional.max_pool2d(
+            x, kernel_size=self.pool_k, stride=self.pool_s, padding=self.pool_p
+        )
 
 
-class HadamardGammaBlockB(torch.nn.Module):
-    def __init__(self, activation: ModuleType):
+class GammaHadamaxFeatureExtractor(torch.nn.Module):
+    def __init__(self, activation=torch.nn.GELU):
         super().__init__()
-        self.convolutional = torch.nn.Conv2d(32, 48, kernel_size=3, stride=2, padding=1)
-        self.normalization = LayerNorm2d(48)
-        self.activation = activation()
+        self.stream = torch.nn.Sequential(
+            HadamaxLayer(
+                4,
+                32,
+                kernel_size=3,
+                padding=1,
+                pool_k=2,
+                pool_s=2,
+                activation=activation,
+            ),
+            HadamaxLayer(
+                32,
+                48,
+                kernel_size=3,
+                padding=1,
+                pool_k=2,
+                pool_s=2,
+                activation=activation,
+            ),
+            HadamaxLayer(
+                48,
+                64,
+                kernel_size=3,
+                padding=0,
+                pool_k=3,
+                pool_s=1,
+                pool_p=1,
+                activation=activation,
+            ),
+            HadamaxLayer(
+                64,
+                64,
+                kernel_size=3,
+                padding=0,
+                pool_k=2,
+                pool_s=2,
+                activation=activation,
+            ),
+            HadamaxLayer(
+                64,
+                64,
+                kernel_size=3,
+                padding=0,
+                pool_k=3,
+                pool_s=1,
+                pool_p=1,
+                activation=activation,
+            ),
+            torch.nn.Flatten(),
+        )
 
     def forward(self, x):
-        x = self.convolutional(x)
-        x = self.normalization(x)
-        x = self.activation(x)
-        return x
-
-
-class HadamardGammaBlockC(torch.nn.Module):
-    def __init__(self, activation: ModuleType):
-        super().__init__()
-        self.convolutional = torch.nn.Conv2d(48, 64, kernel_size=3, stride=1, padding=0)
-        self.normalization = LayerNorm2d(64)
-        self.activation = activation()
-
-    def forward(self, x):
-        x = self.convolutional(x)
-        x = self.normalization(x)
-        x = self.activation(x)
-        return x
-
-
-class HadamardGammaBlockD(torch.nn.Module):
-    def __init__(self, activation: ModuleType):
-        super().__init__()
-        self.convolutional = torch.nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=0)
-        self.normalization = LayerNorm2d(64)
-        self.activation = activation()
-
-    def forward(self, x):
-        x = self.convolutional(x)
-        x = self.normalization(x)
-        x = self.activation(x)
-        return x
-
-
-class HadamardGammaBlockE(torch.nn.Module):
-    def __init__(self, activation: ModuleType):
-        super().__init__()
-        self.convolutional = torch.nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0)
-        self.normalization = LayerNorm2d(64)
-        self.activation = activation()
-
-    def forward(self, x):
-        x = self.convolutional(x)
-        x = self.normalization(x)
-        x = self.activation(x)
-        return x
-
-
-class HadamaxGammaEncoder(torch.nn.Module):
-    def __init__(self, activation: ModuleType = torch.nn.ReLU):
-        super().__init__()
-
-        self.a = HadamardGammaBlockA(activation=activation)
-        self.a_prime = HadamardGammaBlockA(activation=activation)
-
-        self.b = HadamardGammaBlockB(activation=activation)
-        self.b_prime = HadamardGammaBlockB(activation=activation)
-
-        self.c = HadamardGammaBlockC(activation=activation)
-        self.c_prime = HadamardGammaBlockC(activation=activation)
-
-        self.d = HadamardGammaBlockD(activation=activation)
-        self.d_prime = HadamardGammaBlockD(activation=activation)
-
-        self.e = HadamardGammaBlockE(activation=activation)
-        self.e_prime = HadamardGammaBlockE(activation=activation)
-
-    def forward(self, x):
-        a = self.a(x)
-        a_prime = self.a_prime(x)
-        x = a * a_prime
-        x = torch.nn.functional.max_pool2d(x, kernel_size=3, stride=1, padding=1)
-
-        b = self.b(x)
-        b_prime = self.b_prime(x)
-        x = b * b_prime
-        x = torch.nn.functional.max_pool2d(x, kernel_size=3, stride=1, padding=1)
-
-        c = self.c(x)
-        c_prime = self.c_prime(x)
-        x = c * c_prime
-        x = torch.nn.functional.max_pool2d(x, kernel_size=3, stride=1, padding=1)
-
-        d = self.d(x)
-        d_prime = self.d_prime(x)
-        x = d * d_prime
-        x = torch.nn.functional.max_pool2d(x, kernel_size=3, stride=1, padding=1)
-
-        e = self.e(x)
-        e_prime = self.e_prime(x)
-        x = e * e_prime
-        x = torch.nn.functional.max_pool2d(x, kernel_size=3, stride=1, padding=1)
-
-        x = x.flatten(start_dim=1)
-
-        return x
+        return self.stream(x)
