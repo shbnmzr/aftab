be9065936804976da6ed9902d0d8f836a5dae6af
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-06 12:46:20 +0200

    adds skip connections for two layers instead of one

--- aftab/encoders/ResidualGammaEncoder.py
+++ aftab/encoders/ResidualGammaEncoder.py
@@ -1,70 +1,104 @@
 import torch
 from ..common import LayerNorm2d, ModuleType
 
-#####
-# In this file I have used Persian letters for layers instead of naming them with numbers.
-# alef: الف, be: ب, pe: پ, te: ت, se: ث
-#####
+
+class ResBlock(torch.nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        stride: int,
+        conv1: torch.nn.Module,
+        norm1: torch.nn.Module,
+        act1: torch.nn.Module,
+        conv2: torch.nn.Module,
+        norm2: torch.nn.Module,
+    ):
+        super().__init__()
+        self.conv1 = conv1
+        self.norm1 = norm1
+        self.act1 = act1
+        self.conv2 = conv2
+        self.norm2 = norm2
+
+        if in_channels != out_channels or stride != 1:
+            self.shortcut = torch.nn.Sequential(
+                torch.nn.Conv2d(
+                    in_channels, out_channels, kernel_size=1, stride=stride, bias=False
+                ),
+                LayerNorm2d(out_channels),
+            )
+        else:
+            self.shortcut = torch.nn.Identity()
+
+    def forward(self, x):
+        identity = self.shortcut(x)
+
+        out = self.conv1(x)
+        out = self.norm1(out)
+        out = self.act1(out)
+
+        out = self.conv2(out)
+        out = self.norm2(out)
+
+        if identity.shape[2:] != out.shape[2:]:
+            diff_h = identity.shape[2] - out.shape[2]
+            diff_w = identity.shape[3] - out.shape[3]
+            top = diff_h // 2
+            bottom = identity.shape[2] - (diff_h - top)
+            left = diff_w // 2
+            right = identity.shape[3] - (diff_w - left)
+            identity = identity[:, :, top:bottom, left:right]
+
+        out += identity
+        return out
 
 
-class ResidualGammaEncoder(torch.nn.Module):
+class GammaEncoder(torch.nn.Module):
     def __init__(self, *, activation: ModuleType = torch.nn.ReLU):
         super().__init__()
         self.alef = torch.nn.Sequential(
-            torch.nn.Conv2d(4, 32, kernel_size=3, stride=2, padding=1),
+            torch.nn.Conv2d(4, 32, kernel_size=3, stride=2, padding=1, bias=False),
             LayerNorm2d(32),
             activation(),
         )
-        self.be = torch.nn.Sequential(
-            torch.nn.Conv2d(32, 48, kernel_size=3, stride=2, padding=1),
-            LayerNorm2d(48),
-            activation(),
-        )
-        self.pe = torch.nn.Sequential(
-            torch.nn.Conv2d(48, 64, kernel_size=3, stride=1, padding=0),
-            LayerNorm2d(64),
-            activation(),
-        )
-        self.te = torch.nn.Sequential(
-            torch.nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=0),
-            LayerNorm2d(64),
-            activation(),
-        )
-        self.se = torch.nn.Sequential(
-            torch.nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
-            LayerNorm2d(64),
-            activation(),
-        )
 
-        self.alef_projection = torch.nn.Conv2d(4, 32, kernel_size=1, stride=2)
-        self.be_projection = torch.nn.Conv2d(32, 48, kernel_size=1, stride=2)
-        self.pe_projection = torch.nn.Conv2d(48, 64, kernel_size=1, stride=1)
-        self.te_projection = torch.nn.Conv2d(64, 64, kernel_size=1, stride=2)
-        self.se_projection = torch.nn.Conv2d(64, 64, kernel_size=1, stride=1)
+        self.be = ResBlock(
+            in_channels=32,
+            out_channels=64,
+            stride=2,
+            conv1=torch.nn.Conv2d(
+                32, 48, kernel_size=3, stride=2, padding=1, bias=False
+            ),
+            norm1=LayerNorm2d(48),
+            act1=activation(),
+            conv2=torch.nn.Conv2d(
+                48, 64, kernel_size=3, stride=1, padding=0, bias=False
+            ),
+            norm2=LayerNorm2d(64),
+        )
+        self.be_activation = activation()
 
-    def _crop(self, skip, target):
-        _, _, th, tw = target.shape
-        _, _, sh, sw = skip.shape
-        top = (sh - th) // 2
-        left = (sw - tw) // 2
-        return skip[:, :, top : top + th, left : left + tw]
+        self.pe = ResBlock(
+            in_channels=64,
+            out_channels=64,
+            stride=2,
+            conv1=torch.nn.Conv2d(
+                64, 64, kernel_size=3, stride=2, padding=0, bias=False
+            ),
+            norm1=LayerNorm2d(64),
+            act1=activation(),
+            conv2=torch.nn.Conv2d(
+                64, 64, kernel_size=3, stride=1, padding=0, bias=False
+            ),
+            norm2=LayerNorm2d(64),
+        )
+        self.pe_activation = activation()
 
     def forward(self, x):
-        x = self.alef(x) + self.alef_projection(x)
-
-        r = x
-        x = self.be(x) + self.be_projection(r)
-
-        r = x
-        pe_output = self.pe(x)
-        x = pe_output + self._crop(self.pe_projection(r), pe_output)
-
-        r = x
-        te_output = self.te(x)
-        x = te_output + self._crop(self.te_projection(r), te_output)
-
-        r = x
-        se_output = self.se(x)
-        x = se_output + self._crop(self.se_projection(r), se_output)
-
+        x = self.alef(x)
+        x = self.be(x)
+        x = self.be_activation(x)
+        x = self.pe(x)
+        x = self.pe_activation(x)
         return torch.flatten(x, 1)
