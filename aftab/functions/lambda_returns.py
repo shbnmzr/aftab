b19958b6a2a99128b55b61e7d9405e5150ccbeec
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-13 16:29:26 +0200

    renames and simplifies lambda return function

--- aftab/functions/lambda_returns.py
+++ aftab/functions/lambda_returns.py
@@ -3,19 +3,21 @@ import torch
 
 @torch.jit.script
 def lambda_returns(
-    rew,
-    done,
+    rewards,
+    terminations,
     next_q,
     gamma: float,
     lmbda: float,
 ):
-    T = rew.size(0)
-    out = torch.zeros_like(rew)
-    ret = rew[-1] + gamma * next_q[-1] * (1 - done[-1])
-    out[-1] = ret
-    for t in range(T - 2, -1, -1):
+    trajectory_length = rewards.size(0)
+    output = torch.zeros_like(rewards)
+    not_done_last = 1.0 - terminations[-1]
+    accumulated_return = rewards[-1] + gamma * not_done_last * next_q[-1]
+    output[-1] = accumulated_return
+    for t in range(trajectory_length - 2, -1, -1):
+        not_done = 1.0 - terminations[t]
         bootstrap = next_q[t]
-        td_target = rew[t] + gamma * (1 - done[t]) * bootstrap
-        ret = td_target + gamma * lmbda * (1 - done[t]) * (ret - bootstrap)
-        out[t] = ret
-    return out
+        mix = (1.0 - lmbda) * bootstrap + lmbda * accumulated_return
+        accumulated_return = rewards[t] + gamma * not_done * mix
+        output[t] = accumulated_return
+    return output
