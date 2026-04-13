c03db248ea91b91bb36097f8c4ee5ee44ea41b4c
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-13 14:40:50 +0200

    renames target to minimum

--- aftab/common.py
+++ aftab/common.py
@@ -1,15 +1,17 @@
 class LinearEpsilon:
-    def __init__(self, ratio: float = 0.1, target=0.001, maximum: float = 1.0):
+    def __init__(
+        self, ratio: float = 0.1, minimum: float = 0.001, maximum: float = 1.0
+    ):
         self.maximum = maximum
-        self.target = target
+        self.minimum = minimum
         self.ratio = ratio
 
     def get(self, frames, total_frames, all_rewards, episode_returns):
-        target = self.target
+        minimum = self.minimum
         maximum = self.maximum
         decay_duration = total_frames * self.ratio
 
         if decay_duration == 0:
             return maximum
 
-        return max(target, maximum - (frames / decay_duration) * (maximum - target))
+        return max(minimum, maximum - (frames / decay_duration) * (maximum - minimum))
