de23778bfd5d9838c932684ed3dca2f4a94c763e
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-14 14:06:50 +0200

    adds precautionary measures to compilation mixin

--- aftab/mixins/CompilationMixin.py
+++ aftab/mixins/CompilationMixin.py
@@ -6,6 +6,12 @@ class CompilationMixin:
         super().__init__()
 
     def compile_network(self):
-        if not self.should_compile:
+        if not getattr(self, "should_compile", False):
             return
+
+        if not hasattr(self, "_network"):
+            raise AttributeError(
+                "Expected `_network` to be defined before compilation."
+            )
+
         self._network = torch.compile(self._network)
