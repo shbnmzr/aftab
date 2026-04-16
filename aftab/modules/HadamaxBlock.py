a4bef5f30a78b41daa332096b8a7990732967fbb
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-16 13:49:23 +0200

    adds two activations to hadamax layers

--- aftab/modules/HadamaxBlock.py
+++ aftab/modules/HadamaxBlock.py
@@ -1,19 +1,21 @@
 import torch
 from . import LayerNorm2d
+from ..constants import ModuleType
 
 
 class HadamaxBlock(torch.nn.Module):
     def __init__(
         self,
-        in_channels,
-        out_channels,
-        kernel_size,
-        padding,
-        stride,
-        pool_kernel,
-        pool_stride,
-        pool_padding=0,
-        activation=torch.nn.GELU,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        padding: int,
+        stride: int,
+        pool_kernel: int,
+        pool_stride: int,
+        pool_padding: int = 0,
+        chi: ModuleType = torch.nn.GELU,
+        psi: ModuleType = torch.nn.GELU,
     ):
         super().__init__()
         self.conv_a = torch.nn.Conv2d(
@@ -34,16 +36,19 @@ class HadamaxBlock(torch.nn.Module):
         self.norm_a = LayerNorm2d(out_channels)
         self.norm_b = LayerNorm2d(out_channels)
 
-        self.activation = activation()
+        self.chi = chi()
+        self.psi = psi()
 
         self.pool_kernel = pool_kernel
         self.pool_stride = pool_stride
         self.pool_padding = pool_padding
 
     def forward(self, x):
-        a = self.activation(self.norm_a(self.conv_a(x)))
-        b = self.activation(self.norm_b(self.conv_b(x)))
+        a = self.psi(self.norm_a(self.conv_a(x)))
+        b = self.chi(self.norm_b(self.conv_b(x)))
+
         x = a * b
+
         return torch.nn.functional.max_pool2d(
             x,
             kernel_size=self.pool_kernel,
