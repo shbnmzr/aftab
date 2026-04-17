bb1b7932f26543b2a15c5a0e83a0a62e9b0c4ddc
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-17 12:51:45 +0200

    adds type annotations to color intensity

--- aftab/modules/augmentation/ColorIntensity.py
+++ aftab/modules/augmentation/ColorIntensity.py
@@ -2,11 +2,11 @@ import torch
 
 
 class ColorIntensity(torch.nn.Module):
-    def __init__(self, scale=0.05):
+    def __init__(self, scale: float = 0.05):
         super().__init__()
         self.scale = scale
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
         if not self.training:
             return x
         noise = 1.0 + self.scale * torch.randn((x.shape[0], 1, 1, 1), device=x.device)
