77141c1a09cea6cd37adeed3fa9e783a71eb18a0
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-17 10:44:18 +0200

    renames heads to mu

--- aftab/modules/QuantileStream.py
+++ aftab/modules/QuantileStream.py
@@ -6,10 +6,10 @@ class QuantileStream(torch.nn.Module):
         self, feature_dimension: int, action_dimension: int, number_quantiles: int
     ):
         super().__init__()
-        self.linear = torch.nn.Linear(
+        self.mu = torch.nn.Linear(
             feature_dimension, action_dimension * number_quantiles
         )
 
     def forward(self, x, tau_hats):
         B, N = tau_hats.shape
-        return self.linear(x).view(B, N, -1)
+        return self.mu(x).view(B, N, -1)
