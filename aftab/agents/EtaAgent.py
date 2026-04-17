594eadf3b130b4f995abd347295489beab39c809
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-17 13:04:16 +0200

    removes extra unused agent files

--- aftab/agents/EtaAgent.py
+++
@@ -1,8 +0,0 @@
-from .PQNAgent import PQNAgent
-from ..encoders import EtaEncoder
-
-
-class EtaAgent(PQNAgent):
-    def __init__(self, action_dimension: int):
-        super().__init__(action_dimension=action_dimension)
-        self.phi = EtaEncoder()
--- aftab/agents/ThetaAgent.py
+++
@@ -1,8 +0,0 @@
-from .PQNAgent import PQNAgent
-from ..encoders import ThetaEncoder
-
-
-class ThetaAgent(PQNAgent):
-    def __init__(self, action_dimension: int):
-        super().__init__(action_dimension=action_dimension)
-        self.phi = ThetaEncoder()
