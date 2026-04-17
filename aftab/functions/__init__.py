2856044a8e872922404538d6983b1874054be181
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-17 10:28:28 +0200

    removes batched random shift function

--- aftab/functions/__init__.py
+++ aftab/functions/__init__.py
@@ -1,6 +1,5 @@
 from .epsilon_greedy_vectorized import epsilon_greedy_vectorized
 from .flush import flush
 from .mse_loss import mse_loss
-from .batched_random_shift import batched_random_shift
 from .lambda_returns import lambda_returns
 from .lambda_returns_quantile import lambda_returns_quantile
