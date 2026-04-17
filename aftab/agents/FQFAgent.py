8b21c761e226ee32ae01d4a455bf955b076119bd
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-17 10:24:26 +0200

    adds fqf loss

--- aftab/agents/FQFAgent.py
+++ aftab/agents/FQFAgent.py
@@ -1,6 +1,16 @@
+import torch
 from .BaseAgent import BaseAgent
 
 
 class FQFAgent(BaseAgent):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+    def loss(self, predictions, targets, tau_hats):
+        td_error = targets.unsqueeze(1) - predictions.unsqueeze(2)
+        huber = torch.where(
+            td_error.abs() < 1.0, 0.5 * td_error.pow(2), td_error.abs() - 0.5
+        )
+        tau = tau_hats.unsqueeze(2)
+        loss = (torch.abs(tau - (td_error.detach() < 0).float()) * huber).mean()
+        return loss
