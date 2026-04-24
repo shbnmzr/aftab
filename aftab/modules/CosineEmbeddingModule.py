af1479aa7239314d8b5327224f2bc5c0c4965122
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-24 15:08:11 +0200

    fixes cosine embedding module

--- aftab/modules/CosineEmbeddingModule.py
+++ aftab/modules/CosineEmbeddingModule.py
@@ -1,29 +1,22 @@
 import torch
 import math
 from ..constants import ModuleType
-from .Stream import Stream
-
 
 class CosineEmbeddingModule(torch.nn.Module):
     def __init__(
         self,
         *,
         embedding_dimension: int,
+        feature_dimension: int,
         activation: ModuleType = torch.nn.ReLU,
     ):
         super().__init__()
         self.embedding_dimension = embedding_dimension
-        self.cosine_network = Stream(
-            input_dimension=embedding_dimension,
-            embedding_dimension=embedding_dimension,
-            output_dimension=embedding_dimension,
-            normalization=False,
-        )
         pi_indices = math.pi * torch.arange(1, embedding_dimension + 1).float()
         self.register_buffer("pi_indices", pi_indices)
+        self.linear = torch.nn.Linear(embedding_dimension, feature_dimension)
         self.activation = activation()
 
-    def forward(self, fractions: torch.Tensor):
+    def forward(self, fractions: torch.Tensor) -> torch.Tensor:
         cos = torch.cos(fractions.unsqueeze(-1) * self.pi_indices.view(1, 1, -1))
-        embeddings = self.cosine_network(cos)
-        return self.activation(embeddings)
+        return self.activation(self.linear(cos))
