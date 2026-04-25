16506477c671557f373f185abb28b4a4b136af8d
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-25 13:54:48 +0200

    removes extra functions

--- aftab/functions/__init__.py
+++ aftab/functions/__init__.py
@@ -2,5 +2,3 @@ from .epsilon_greedy_vectorized import epsilon_greedy_vectorized
 from .flush import flush
 from .mse_loss import mse_loss
 from .lambda_returns import lambda_returns
-from .random_shifts import RandomShiftsAug
-from .n_step_returns import n_step_returns
