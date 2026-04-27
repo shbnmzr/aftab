96ba505d755865ddfc6dfb03f9156c19807b3b45
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-27 17:43:00 +0200

    fixes LayerNorm2d

--- aftab/modules/LayerNorm2d.py
+++ aftab/modules/LayerNorm2d.py
@@ -1,11 +1,19 @@
 import torch
 
 
-class LayerNorm2d(torch.nn.GroupNorm):
-    def __init__(self, num_channels: int):
-        super().__init__(
-            num_groups=1,
-            num_channels=num_channels,
-            eps=1e-6,
-            affine=True,
-        )
+class LayerNorm2d(torch.nn.Module):
+    def __init__(self, num_channels: int, eps: float = 1e-6):
+        super().__init__()
+        self.weight = torch.nn.Parameter(torch.ones(num_channels))
+        self.bias = torch.nn.Parameter(torch.zeros(num_channels))
+        self.eps = eps
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if x.dim() != 4:
+            raise Exception("LayerNorm2d input must be four dimensional.")
+
+        mean = x.mean(dim=1, keepdim=True)
+        var = (x - mean).pow(2).mean(dim=1, keepdim=True)
+        x = (x - mean) / torch.sqrt(var + self.eps)
+        x = x * self.weight.view(1, -1, 1, 1) + self.bias.view(1, -1, 1, 1)
+        return x
