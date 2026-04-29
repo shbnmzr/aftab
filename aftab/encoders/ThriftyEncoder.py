8a030c46af5d4159d8ea4b336d603a52fc12a0b4
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-29 15:10:01 +0200

    fixes thrifty encoder typos

--- aftab/encoders/ThriftyEncoder.py
+++ aftab/encoders/ThriftyEncoder.py
@@ -1,4 +1,5 @@
 import torch
+
 from ..modules import LayerNorm2d
 from ..constants import ModuleType
 
@@ -16,27 +17,37 @@ class ThriftyEncoder(torch.nn.Module):
         self.filters = filters
         self.iterations = iterations
         self.conv = torch.nn.Conv2d(
-            filters, filters, kernel_size=kernel_size, padding=1, bias=False
+            filters,
+            filters,
+            kernel_size=kernel_size,
+            padding=kernel_size // 2,
+            bias=False,
         )
         self.activation = activation()
         self.normalizations = torch.nn.ModuleList(
             [LayerNorm2d(filters) for _ in range(iterations)]
         )
-        self.pool_strategy = [iterations // 5 * i for i in range(1, 5)]
+        alpha = torch.zeros(iterations, 2)
+        alpha[:, 0] = 0.1
+        alpha[:, 1] = 0.9
+        self.alpha = torch.nn.Parameter(alpha)
+
+        pool_every = max(1, iterations // 5)
+        self.pool_strategy = [
+            t % pool_every == pool_every - 1 for t in range(iterations)
+        ]
         self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
         self.global_pool = torch.nn.AdaptiveMaxPool2d((1, 1))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         if self.filters > x.size(1):
-            x = torch.torch.nn.functional.pad(
-                x, (0, 0, 0, 0, 0, self.filters - x.size(1))
-            )
+            x = torch.nn.functional.pad(x, (0, 0, 0, 0, 0, self.filters - x.size(1)))
 
         for t in range(self.iterations):
             a_t = self.activation(self.conv(x))
-            x = x + a_t
+            x = self.alpha[t, 0] * a_t + self.alpha[t, 1] * x
             x = self.normalizations[t](x)
-            if t in self.pool_strategy:
+            if self.pool_strategy[t]:
                 x = self.pool(x)
 
         x = self.global_pool(x)
