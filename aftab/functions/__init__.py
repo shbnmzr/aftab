cf91063c98f2cfe276945fddb8af4bf67baed2d2
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-29 17:10:36 +0200

    removes extra import

--- aftab/functions/__init__.py
+++ aftab/functions/__init__.py
@@ -2,4 +2,3 @@ from .epsilon_greedy_vectorized import epsilon_greedy_vectorized
 from .flush import flush
 from .mse_loss import mse_loss
 from .lambda_returns import lambda_returns
-from .positional_embedding_sincos_2d import positional_embedding_sincos_2d
