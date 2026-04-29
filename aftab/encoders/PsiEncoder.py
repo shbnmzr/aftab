b8c3fc07a00a675c79f5d62a9a7319f0f7365f20
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-29 16:25:12 +0200

    fixes omega and psi encoders

--- aftab/encoders/PsiEncoder.py
+++ aftab/encoders/PsiEncoder.py
@@ -1,6 +1,14 @@
+import torch
 from thriftynet import ThriftyEncoder
+from ..modules import LayerNorm2d
 
 
-class PsiEncoder(ThriftyEncoder):
+class OmegaEncoder(ThriftyEncoder):
     def __init__(self):
-        super().__init__(filters=64, kernel_size=3, iterations=20)
+        super().__init__(
+            filters=64,
+            iterations=20,
+            kernel_size=3,
+            normalization=LayerNorm2d,
+            activation=torch.nn.ReLU,
+        )
