311214f7c47638d812d37bc2c7e2570de799677b
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-29 18:23:44 +0200

    removes thrifty variants

--- aftab/maps/encoders.py
+++ aftab/maps/encoders.py
@@ -10,8 +10,6 @@ from ..encoders import ThetaEncoder
 from ..encoders import HadamaxNatureDQNEncoder
 from ..encoders import HadamaxGammaEncoderV1
 from ..encoders import HadamaxGammaEncoderV2
-from ..encoders import PsiEncoder
-from ..encoders import OmegaEncoder
 
 encoders_map = {
     "nature": NatureDQNEncoder,
@@ -31,6 +29,4 @@ encoders_map = {
     "gammahadamaxv1": HadamaxGammaEncoderV1,
     "hadamaxgammav2": HadamaxGammaEncoderV2,
     "gammahadamaxv2": HadamaxGammaEncoderV2,
-    "psi": PsiEncoder,
-    "omega": OmegaEncoder,
 }
