cff745628fe0f663a9393d660b98bacefc194c30
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-29 18:22:10 +0200

    removes thrifty variants

--- aftab/encoders/__init__.py
+++ aftab/encoders/__init__.py
@@ -10,6 +10,3 @@ from .ThetaEncoder import ThetaEncoder
 from .HadamaxGammaEncoderV1 import HadamaxGammaEncoderV1
 from .HadamaxGammaEncoderV2 import HadamaxGammaEncoderV2
 from .HadamaxNatureDQNEncoder import HadamaxNatureDQNEncoder
-from .HadamaxThriftyEncoder import HadamaxThriftyEncoder
-from .PsiEncoder import PsiEncoder
-from .OmegaEncoder import OmegaEncoder
