623f2ed85ca3a800322076cb4bff133de670d7fb
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-30 16:23:51 +0200

    adds required padding to nature dqn encoder

--- aftab/encoders/NatureDQNEncoder.py
+++ aftab/encoders/NatureDQNEncoder.py
@@ -7,9 +7,15 @@ class NatureDQNEncoder(torch.nn.Module):
     def __init__(self, *, activation: ModuleType = torch.nn.ReLU):
         super().__init__()
         self.stream = torch.nn.Sequential(
-            EncoderBlock(4, 32, kernel_size=8, stride=4, activation=activation),
-            EncoderBlock(32, 64, kernel_size=4, stride=2, activation=activation),
-            EncoderBlock(64, 64, kernel_size=3, stride=1, activation=activation),
+            EncoderBlock(
+                4, 32, kernel_size=8, stride=4, padding=0, activation=activation
+            ),
+            EncoderBlock(
+                32, 64, kernel_size=4, stride=2, padding=0, activation=activation
+            ),
+            EncoderBlock(
+                64, 64, kernel_size=3, stride=1, padding=0, activation=activation
+            ),
             torch.nn.Flatten(),
         )
 
