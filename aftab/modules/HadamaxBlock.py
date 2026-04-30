313aef15cf122978eaebb841de90646b24546faf
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-30 16:29:32 +0200

    speeds up the hadamax block

--- aftab/modules/HadamaxBlock.py
+++ aftab/modules/HadamaxBlock.py
@@ -1,5 +1,4 @@
 import torch
-from .LayerNorm2d import LayerNorm2d
 from ..constants import ModuleType
 
 
@@ -26,29 +25,33 @@ class HadamaxBlock(torch.nn.Module):
             stride=stride,
             padding=padding,
         )
-
-        self.alpha = LayerNorm2d(out_channels)
-        self.beta = LayerNorm2d(out_channels)
-
-        self.chi = chi()
-        self.psi = psi()
+        self.normalization = torch.nn.GroupNorm(
+            num_groups=2, num_channels=out_channels * 2
+        )
 
         self.pool_kernel = pool_kernel
         self.pool_stride = pool_stride
         self.pool_padding = pool_padding
 
+        self.same_acts = chi == psi
+        if self.same_acts:
+            self.activation = chi()
+        else:
+            self.chi = chi()
+            self.psi = psi()
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x_fused = self.fused(x)
-        a, b = torch.chunk(x_fused, 2, dim=1)
-
-        a = self.alpha(a)
-        a = self.psi(a)
-
-        b = self.beta(b)
-        b = self.chi(b)
+        x = self.fused(x)
+        x = self.normalization(x)
+        if self.same_acts:
+            x = self.activation(x)
+            a, b = torch.chunk(x, 2, dim=1)
+        else:
+            a, b = torch.chunk(x, 2, dim=1)
+            a = self.psi(a)
+            b = self.chi(b)
 
         x = a * b
-
         return torch.nn.functional.max_pool2d(
             x,
             kernel_size=self.pool_kernel,
