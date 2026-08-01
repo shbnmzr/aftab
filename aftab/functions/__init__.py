0956c52ad4c145d115fdfeafc75405fa40c380cc
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-01 17:47:47 +0200

    remove gompertz function and its import

--- aftab/functions/__init__.py
+++ aftab/functions/__init__.py
@@ -2,5 +2,4 @@ from .epsilon_greedy_vectorized import epsilon_greedy_vectorized
 from .flush import flush
 from .mse_loss import mse_loss
 from .lambda_returns import lambda_returns
-from .gompertz import gompertz
 from .random_activation_function import random_activation_function
--- aftab/functions/gompertz.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-
-def gompertz(x: torch.Tensor) -> torch.Tensor:
-    return torch.exp(-torch.exp(-x))
