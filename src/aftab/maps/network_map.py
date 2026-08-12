08fa86bd43952c57e394878bea564e50e2e37179
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-12 15:54:29 +0200

    Fix naming inconsistencies in network imports and update network_map entries

--- src/aftab/maps/network_map.py
+++ src/aftab/maps/network_map.py
@@ -1,23 +1,23 @@
 from aftab.networks import PQNNetwork
-from aftab.networks import DuellingNetwork
+from aftab.networks import DuelingNetwork
 from aftab.networks import BootstrappedNetwork
-from aftab.networks import BootstrappedDuellingNetwork
+from aftab.networks import BootstrappedDuelingNetwork
 from aftab.networks import DistributionalNetwork
-from aftab.networks import DistributionalDuellingNetwork
-from aftab.networks import DistributionalBootstrappedDuellingNetwork
+from aftab.networks import DistributionalDuelingNetwork
+from aftab.networks import DistributionalBootstrappedDuelingNetwork
 
 network_map = {
     "q": PQNNetwork,
-    "duelling": DuellingNetwork,
+    "duelling": DuelingNetwork,
     "bootstrapped": BootstrappedNetwork,
-    "bootstrapped-duelling": BootstrappedDuellingNetwork,
+    "bootstrapped-duelling": BootstrappedDuelingNetwork,
     "distributional": DistributionalNetwork,
-    "distributional-duelling": DistributionalDuellingNetwork,
-    "distributional-bootstrapped-duelling": DistributionalBootstrappedDuellingNetwork,
-    "bootstrapped-distributional-duelling": DistributionalBootstrappedDuellingNetwork,
-    "d": DuellingNetwork,
-    "bdd": DistributionalBootstrappedDuellingNetwork,
-    "bd": BootstrappedDuellingNetwork,
-    "dd": DistributionalDuellingNetwork,
+    "distributional-duelling": DistributionalDuelingNetwork,
+    "distributional-bootstrapped-duelling": DistributionalBootstrappedDuelingNetwork,
+    "bootstrapped-distributional-duelling": DistributionalBootstrappedDuelingNetwork,
+    "d": DuelingNetwork,
+    "bdd": DistributionalBootstrappedDuelingNetwork,
+    "bd": BootstrappedDuelingNetwork,
+    "dd": DistributionalDuelingNetwork,
     "b": BootstrappedNetwork,
 }
