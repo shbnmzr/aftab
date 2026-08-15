8be2cf1ec78bf17708b5d6bd995bcc05c1a88798
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-15 17:04:06 +0200

    Enhance argument parsing in run_experiments.py with detailed help descriptions and add verbose flag

--- examples/run_experiments.py
+++ examples/run_experiments.py
@@ -1,24 +1,39 @@
 import argparse
 from aftab import Aftab
 
+
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--verbose",
+        type=int,
+        required=True,
+        default=True,
+        help="Verbose makes the agent to flush out periodical output as feedback.",
+    )
     parser.add_argument(
         "--seed",
         type=int,
         required=True,
-        help="Aftab running seed"
+        help="Seed controls the reproducibility of the experiments. It seeds Numpy, PyTorch, Python Random module, and Scikit Learn libraey with the same seed.",
     )
     parser.add_argument(
         "--environment",
         type=str,
         required=True,
-        help="Environment name"
+        help="EnvPool environment name, this can be any environment listed on their website under Atari benchmark or Procgen.",
+    )
+    parser.add_argument(
+        "--name",
+        type=str,
+        required=True,
+        help="Required experiment name that will eventually be used to store results on the disk drive.",
     )
     args = parser.parse_args()
-    agent = Aftab(verbose=True)
+    agent = Aftab(experiment_name=args.name, verbose=args.verbose)
     agent.train(environment=args.environment, seed=args.seed)
     agent.log(directory="results")
+
 
 if __name__ == "__main__":
     main()
